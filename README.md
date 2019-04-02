01661ced0694a0f06c1476e5e5b17af6d1ea7beb
ryanpbuss <ryan.p.buss@gmail.com>
2019-04-02 15:34:26 -0700

    First commit.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# CompassBelt
+# Compass Belt Project
+Author: Ryan Buss
+
+An experimental project to build a compass belt.
+
+
+Created 2 March 2019
+
---
+++ sketch_apr02a/sketch_apr02a.ino
@@ -0,0 +1,15 @@
+// Testing Playground
+// Used to test components
+// Author: Ryan Buss
+// March 2019
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
