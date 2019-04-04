bdb9ff5943e6b421d9e8e3a3f61b10e84e4d0d0a
ryanpbuss <ryan.p.buss@gmail.com>
2019-04-04 12:02:50 -0700

    finally working, did not realize the rails were divided on the breadboard. Few code fixes and restructuring.

--- ShiftLED_Test/ShiftLED_Test.ino
+++ ShiftLED_Test/ShiftLED_Test.ino
@@ -21,14 +21,26 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-  //pinMode(masterReset, OUTPUT);
+  pinMode(masterReset, OUTPUT);
   //digitalWrite(masterReset, HIGH);
   //Serial.begin(9600);
   //Serial.println("reset");
   
 }
 
-void loop() {
+void shiftWritePin(int Pin = 1, boolean State = true){
+  byte Data = 0;
+  digitalWrite(latchPin, LOW);
+  bitWrite(Data, Pin, State);
+  shiftOut(dataPin, clockPin, MSBFIRST, Data);
+  
+  digitalWrite(latchPin, HIGH);
+  // this would leave the latch pin low, and only pop it to high after a write
+  // digitalWrite(latchPin, LOW);
+}
+
+void binaryCounter(int switchDelay = 250) {
+  digitalWrite(masterReset, HIGH);
   for (int j = 0; j < 256; j++) {
     //ground latchPin and hold low for as long as you are transmitting
     digitalWrite(latchPin, LOW);
@@ -36,11 +48,55 @@ void loop() {
     //return the latch pin high to signal chip that it
     //no longer needs to listen for information
     digitalWrite(latchPin, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(150);
-    digitalWrite(LED_BUILTIN, LOW);
+    delay(switchDelay);
+    //digitalWrite(LED_BUILTIN, HIGH);
+    //delay(150);
+    //digitalWrite(LED_BUILTIN, LOW);
   }
+}
+
+
+void sequentialLight(int switchDelay = 250){
+  // clear whatever's there
+  digitalWrite(masterReset, LOW);
+  digitalWrite(masterReset, HIGH);
+
+  int pinNum = 0;
+  for (pinNum = 0; pinNum < 8; pinNum++){
+    shiftWritePin(pinNum, true);
+    delay(switchDelay);
+  }
+  for (pinNum = 7; pinNum >= 0; pinNum--){
+    shiftWritePin(pinNum, true);
+    delay(switchDelay);
+  }
+
+  
+  /*
+  digitalWrite(latchPin, LOW);
+  int i;
+  for (i = 1; i < 257; i << 1) {
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, i);
+    digitalWrite(latchPin, HIGH);
+    delay(switchDelay);
+  }
+  for (i = 256; i > 0 ; i >> 1) {
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, i);
+    digitalWrite(latchPin, HIGH);
+    delay(switchDelay);
+  }
+  */
+}
+
+
+void loop() {
+  //binaryCounter(75);
+  //delay(500);
+  sequentialLight(250);
+  delay(500);
+
   /*
   digitalWrite(clockPin, LOW);
 
