5fb2d539f87de83ef5e2220a7c4afd4c8f3255cd
ryanpbuss <ryan.p.buss@gmail.com>
2019-04-03 21:19:59 -0700

    more mucking, still unsuccessful. maybe wiring?

--- ShiftLED_Test/ShiftLED_Test.ino
+++ ShiftLED_Test/ShiftLED_Test.ino
@@ -21,24 +21,43 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-  pinMode(masterReset, OUTPUT);
-  digitalWrite(masterReset, HIGH);
+  //pinMode(masterReset, OUTPUT);
+  //digitalWrite(masterReset, HIGH);
   //Serial.begin(9600);
   //Serial.println("reset");
   
 }
 
 void loop() {
+  for (int j = 0; j < 256; j++) {
+    //ground latchPin and hold low for as long as you are transmitting
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, j);
+    //return the latch pin high to signal chip that it
+    //no longer needs to listen for information
+    digitalWrite(latchPin, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(150);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  /*
+  digitalWrite(clockPin, LOW);
 
-  digitalWrite(masterReset, HIGH);
+  //digitalWrite(masterReset, HIGH);
   digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, 1);
+  delay(10);
+  shiftOut(dataPin, clockPin, LSBFIRST, 255);
   digitalWrite(latchPin, HIGH);
+  //delay(10);
+  //digitalWrite(latchPin, LOW);
+  
   delay(1500);
-  digitalWrite(masterReset, LOW);
+  //digitalWrite(masterReset, LOW);
   digitalWrite(LED_BUILTIN, LOW);
   delay(500);
+  */
   
   
   /*
